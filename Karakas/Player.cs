6ddddc9d15b17bbf15b9f3aa9d88e60a5b271a6d
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2016-12-28 00:37:11 -0600

    A_Player_can_have_no_opponents

---
+++ Karakas/Player.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Karakas
+{
+    public class Player
+    {
+        public ICollection<Player> Opponents { get; }
+
+        public Player()
+        {
+            Opponents = new List<Player>();
+        }
+    }
+}
---
+++ KarakasTests/KarakasTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Karakas;
+using Xunit;
+
+namespace KarakasTests
+{
+    public class KarakasTests
+    {
+        [Fact]
+        public void A_Player_can_have_no_opponents()
+        {
+            var player = new Player();
+            player.Opponents.Should().BeEmpty();
+        }
+    }
+}
