66869cb1eacaad0e2899a2866d9dec690fb5077d
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2016-12-28 20:22:43 -0600

    remove player logic an tests

--- Karakas/Player.cs
+++ Karakas/Player.cs
@@ -4,17 +4,8 @@ namespace Karakas
 {
     public class Player
     {
-
-        public ICollection<Player> Opponents { get; }
-
         public Player()
         {
-            Opponents = new List<Player>();
-        }
-
-        public Player(Player opponent)
-        {
-            Opponents = new List<Player> {opponent};
         }
     }
 }
--- KarakasTests/GameTests.cs
+++ KarakasTests/GameTests.cs
@@ -15,14 +15,14 @@ namespace KarakasTests
         [Fact]
         public void A_two_player_game_is_one_that_has_only_two_players()
         {
-            Game game = GameFactory.CreateTwoPlayerGame();
+            var game = GameFactory.CreateTwoPlayerGame();
             game.Players.Should().Be(2);
         }
 
         [Fact]
         public void A_multiplayer_game_is_one_that_starts_with_more_than_two_players()
         {
-            Game game = GameFactory.CreateMultiplayerGame();
+            var game = GameFactory.CreateMultiplayerGame();
             game.Players.Should().BeGreaterThan(2);
         }
 
@@ -42,24 +42,8 @@ namespace KarakasTests
         [InlineData(5)]
         public void A_multiplayer_game_should_always_start_with_multiple_players(int numberOfPlayers)
         {
-            Game game = GameFactory.CreateMultiplayerGame(numberOfPlayers);
+            var game = GameFactory.CreateMultiplayerGame(numberOfPlayers);
             game.Players.Should().Be(numberOfPlayers);
         }
-
-        [Fact]
-        public void A_Player_can_have_no_opponents()
-        {
-            var player = new Player();
-            player.Opponents.Should().BeEmpty();
-        }
-
-        [Fact]
-        public void A_Player_can_have_one_Opponent()
-        {
-            var opponent = new Player();
-            var player = new Player(opponent);
-            player.Opponents.Count.Should().Be(1);
-        }
-
     }
 }
